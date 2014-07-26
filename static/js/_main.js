82465e8ea03e06c8ae2e26dab91c0c71f55b44a8
Maverick Chan <guanhao3797@gmail.com>
2014-07-27 01:44:08 +0800

    Added photos and layout

--- static/js/_main.js
+++ static/js/_main.js
@@ -78,7 +78,7 @@
 		// Build HTML for Nav
 		$('<div/>', {
 		    'id' : 'slideNav'
-		}).append($('<ul><li class="slideNavPrev"><a class="disabled" href="#" title="Go to previous slide"><span class="ico ico-up">↑</span></a></li><li><span id="activeSlide">'+activeSlide+'</span>/<span id="maxSlides">'+slidesCount+'</span></li><li class="slideNavNext"><a href="#" title="Go to next slide"><span class="ico ico-down">↓</span></a></li></ul>')).appendTo('body').delay(1200).fadeIn(duration);
+		}).append($('<ul><li class="slideNavPrev"><a class="disabled" href="#" title="Go to previous slide"><span class="ico ico-up">↑</span></a></li><li><span id="activeSlide">'+activeSlide+'</span>/<span id="maxSlides">'+slidesCount+'</span></li><li id="nextButton" class="slideNavNext"><a href="#" title="Go to next slide"><span class="ico ico-down">↓</span></a></li></ul>')).appendTo('body').delay(1200).fadeIn(duration);
 		
 
 		// Navigation highligting
